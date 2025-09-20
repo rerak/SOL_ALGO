e2940d7c451b4588cce1dfc8a2e67019e5b554ed
rererererak <144878641+rerak@users.noreply.github.com>
2025-09-20 23:09:19 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 5일차 - 배열 최소 합, Time: 103 ms, Memory: 60,160 KB -BaekjoonHub

--- SWEA/D2/4881. ［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합/［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합.py
+++ SWEA/D2/4881. ［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합/［파이썬 S／W 문제해결 기본］ 5일차 － 배열 최소 합.py
@@ -1,25 +1,26 @@
-
-def dfs(depth, total, used):
+def backtrack(idx, total):
     global min_sum
 
     if total >= min_sum:
         return
 
-    if depth == N:
+    if idx == N:
         min_sum = min(min_sum, total)
         return
 
-    for i in range(N):
-        if not used[i]:
-            used[i] = True
-            dfs(depth + 1, total + arr[depth][i], used)
-            used[i] = False
+    for j in range(N):
+        if not visited[j]:
+            visited[j] = True
+            backtrack(idx + 1, total + arr[idx][j])
+            visited[j] = False
 
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
+
     min_sum = float('inf')
-    used = [False] * N
-    dfs(0, 0, used)
+    visited = [False] * N
+    backtrack(0, 0)
+
     print(f"#{tc} {min_sum}")
