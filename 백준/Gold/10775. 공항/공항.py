982fbfd8ef4d5bf6005b6501ced79b454754b4d1
rererererak <144878641+rerak@users.noreply.github.com>
2025-10-16 17:45:46 +0900

    [Gold II] Title: 공항, Time: 216 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    x = find(a)
+    y = find(b)
+    if x > y:
+        parent[x] = y
+    else:
+        parent[y] = x
+
+G = int(input())
+P = int(input())
+
+parent = [x for x in range(G+1)]
+
+plane = 0
+for _ in range(P):
+    gate = int(input())
+    
+    boarding_number = find(gate)
+
+    if boarding_number == 0:
+        break
+    union(boarding_number, boarding_number-1)
+    plane += 1
+
+print(plane)
