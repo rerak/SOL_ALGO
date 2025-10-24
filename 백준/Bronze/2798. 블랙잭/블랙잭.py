8ee3f0054e2ada1f0c94f96ff16220582228b4ee
rererererak <144878641+rerak@users.noreply.github.com>
2025-10-24 20:53:01 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,16 +1,12 @@
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 
-arr.sort()
-
 max_cnt = 0
+cnt = 0
 for i in range(N-2):
     for j in range(i+1, N-1):
-        cnt = 0
         for x in range(j+1, N):
             cnt = arr[i]+arr[j]+arr[x]
-
-
             if cnt <= M:
                 max_cnt = max(max_cnt,cnt)
 print(max_cnt)
