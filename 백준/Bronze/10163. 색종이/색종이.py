a1960daacb83b7452db49772f4f0e58a6f9c619c
rererererak <144878641+rerak@users.noreply.github.com>
2025-10-25 16:34:52 +0900

    [Bronze I] Title: 색종이, Time: 460 ms, Memory: 39328 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,10 +1,10 @@
+
 N = int(input()) # 색종이 갯수
 arr = [[0]*1001 for _ in range(1001)]
-for t in range(1, N+1):
+for n in range(1, N+1):
     a, b, c, d = map(int, input().split())
     for i in range(b, b+d):
-        for j in range(a, a+c):
-            arr[i][j] = t
+        arr[i][a:a+c] = [n]*c
 cnt = [0] * (N+1)
 for lst in arr:
     for n in lst:
